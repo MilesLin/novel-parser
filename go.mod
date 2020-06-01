ff465aef770a4e22dcacd1787583eb797948bff2
Miles <milesasp@gmail.com>
2020-06-01 23:28:58 +0800

    將檔案轉成 epub

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/PuerkitoBio/goquery v1.5.1
 	github.com/SebastiaanKlippert/go-wkhtmltopdf v1.5.0
+	github.com/bmaupin/go-epub v0.5.3
 	github.com/jung-kurt/gofpdf v1.16.2
 	github.com/phpdave11/gofpdi v1.0.12 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"github.com/bmaupin/go-epub"
 	"golang.org/x/net/html/charset"
 	"golang.org/x/text/transform"
+	"html/template"
 	"io"
 	"io/ioutil"
 	"log"
@@ -17,8 +19,46 @@ import (
 )
 
 func main() {
-
 	//DownloadPageToFile()
+
+	// Create a new EPUB
+	e := epub.NewEpub("施落衛琮曦-錦繡小福妻")
+
+	// Set the author
+	e.SetAuthor("三妖")
+	e.SetDescription("穿越成宰相庶女的施落，還沒過一天好日子，就被老皇帝賜給貶為庶人雙腿殘廢的衛家小王爺……醒來後，沒有丫環，沒有錦衣玉食，只有一個雙腿殘廢的帥哥陰鷙的看著她。“休書我不會寫，你想死隨時都可以，生是衛家人，死也是衛家的鬼！”施落看著窮的只剩老鼠的家，為了能吃飽穿暖活下去，只能想辦法賺錢養家，賺錢養夫，賺錢養娃…衛小王爺多疑敏感不好養，原以為養成了一只白眼狼，誰知道一朝功成名就，他居然帶人堵上門。“听說你跟別人說我死了？”施落︰“呵呵……誤會！”某王爺將門一關，“那我這個死人再娶你一次可好？”")
+	e.SetTitle("施落衛琮曦-錦繡小福妻")
+
+	// Add a section
+	b, err := ioutil.ReadFile("temp.txt")
+	if err != nil {
+		log.Fatal("Could not read the file %v", err)
+	}
+	var model Model
+	json.Unmarshal(b, &model)
+	for _, v := range model.Data {
+
+		section1Body := `<h2>{{.Title}}</h2>{{range .Content}}<p>{{.}}</p>{{end}}`
+		var tpl bytes.Buffer
+		temp := template.New("novel")
+		t, err := temp.Parse(section1Body)
+		if err != nil {
+			log.Fatalf("Could not parse %v", err)
+		}
+		err = t.Execute(&tpl, v)
+		if err != nil {
+			log.Fatalf("Could not Execute %v", err)
+		}
+
+		e.AddSection(string(tpl.Bytes()), v.Title, "", "")
+		fmt.Println(v.Title)
+	}
+
+	// Write the EPUB
+	err = e.Write("施落衛琮曦-錦繡小福妻.epub")
+	if err != nil {
+		log.Fatalf("Could not wirte to epub %v", err)
+	}
 
 }
 
